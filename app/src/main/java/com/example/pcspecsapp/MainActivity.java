ea2d6397d975d62de802e71265519dd06e18fa9b
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2024-02-21 19:34:54 +0100

    done for today

--- app/src/main/java/com/example/pcspecsapp/MainActivity.java
+++ app/src/main/java/com/example/pcspecsapp/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.pcspecsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+
+        PcSpecs pcSpecs = ApiLayer.getPcSpecsById(1);
+        text.setText(pcSpecs.getCpu());
     }
 }
