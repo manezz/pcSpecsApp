97ea3bc5332e84d3b9911b0cb9b4f9a657a39378
Magnus V.Norling <68356591+manezz@users.noreply.github.com>
2024-02-22 09:44:53 +0100

    works internet

--- app/src/main/java/com/example/pcspecsapp/ServiceBuilder.java
+++ app/src/main/java/com/example/pcspecsapp/ServiceBuilder.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceBuilder {
-    private static final String URL = "http://10.0.2.2:8080/JavaAPI1/api/";
+    private static final String URL = "http://10.0.2.2:8080/api/";
     private static Retrofit retrofit =
             new Retrofit.Builder().baseUrl(URL)
                     .addConverterFactory(GsonConverterFactory.create()).build();
